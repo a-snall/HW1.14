d327299f4bc13b39d3a70ef59158889c3750d7ca
Astrid Snäll <vaski2astrid@hotmail.com>
2023-02-12 14:21:02 -0500

    Initial Commit

---
+++ HW1.14/HW1_14App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1_14App.swift
+//  HW1.14
+//
+//  Created by Astrid Snäll on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HW1_14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
